4288cf67089ae00e7784aaa2121abe4873048f03
wdxxs2z <243219650@qq.com>
2018-03-14 02:59:56 -0700

    add kubernetes flag env.

--- docker/bin/run.sh
+++ docker/bin/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+if [ $KUBERNETES_ENV == 'false' || $KUBERNETES_ENV == '' ]; then
+    /usr/local/uaa/bin/job_properties.sh
+fi
+
 if [ ! -f /.pre_start_created ]; then
     /usr/local/uaa/bin/pre-start.sh
 fi
-
-/usr/local/uaa/bin/job_properties.sh
 
 exec catalina.sh run
